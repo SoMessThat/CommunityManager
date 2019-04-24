43855d69b08365a2f153b447d434bae83a8d560e
wukaibin <842387850@qq.com>
2019-04-24 09:39:20 +0800

    缴费代码生成

---
+++ src/main/java/mess/wkb/cm/code/ctrl/CmPaymentCtrl.java
@@ -0,0 +1,163 @@
+package mess.wkb.cm.code.ctrl;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import mess.wkb.cm.code.po.CmPaymentPO;
+import mess.wkb.cm.code.service.CmPaymentService;
+import mess.wkb.cm.tool.bean.Paged;
+import mess.wkb.cm.tool.util.ObjectUtil;
+import mess.wkb.cm.tool.util.ajax.Response;
+import mess.wkb.cm.tool.util.ajax.ResponseFactory;
+import mess.wkb.cm.tool.web.MysqlDBException;
+
+
+@Controller
+@RequestMapping("/CmPayment") 
+public class CmPaymentCtrl {
+
+	@Autowired
+	private CmPaymentService cmPaymentService;
+	
+	SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+	@RequestMapping(value ="/queryPageCmPayment")
+	@ResponseBody
+	public Response<List<CmPaymentPO>> queryPageCmPayment(Integer page,Integer limit,HttpServletRequest request) throws ParseException{
+		Response<List<CmPaymentPO>> response = ResponseFactory.getDefaultSuccessResponse();
+		Paged<CmPaymentPO> cmPayments = null;
+		
+		
+		CmPaymentPO condition=new CmPaymentPO();
+        String id = request.getParameter("cmPayment_id");
+		if(!ObjectUtil.isEmpty(id)) condition.setId(Long.valueOf(id));
+        String name = request.getParameter("cmPayment_name");
+		if(!ObjectUtil.isEmpty(name)) condition.setName(String.valueOf(name));
+        String creatTime = request.getParameter("cmPayment_creatTime");
+		if(!ObjectUtil.isEmpty(creatTime)) condition.setCreatTime(dateFormat.parse(creatTime));
+        String departmentId = request.getParameter("cmPayment_departmentId");
+		if(!ObjectUtil.isEmpty(departmentId)) condition.setDepartmentId(String.valueOf(departmentId));
+        String userName = request.getParameter("cmPayment_userName");
+		if(!ObjectUtil.isEmpty(userName)) condition.setUserName(String.valueOf(userName));
+        String state = request.getParameter("cmPayment_state");
+		if(!ObjectUtil.isEmpty(state)) condition.setState(String.valueOf(state));
+	
+		try {
+			cmPayments = cmPaymentService.queryPageCmPayment(page,limit,condition);
+		} catch (MysqlDBException e) {
+			e.printStackTrace();
+			response.setError("网络连接失败，请检查网络");
+		}
+		response.setData(cmPayments.getListData());
+		response.setCount(cmPayments.getTotalHit());
+		response.setResult(Response.RESULT_SUCCESS);
+		return response;
+		
+	}
+	
+	@RequestMapping(value="/delCmPayment")
+	@ResponseBody
+	public Response<CmPaymentPO> delCmPayment(String id){
+		Response<CmPaymentPO> response = ResponseFactory.getDefaultSuccessResponse();
+
+		if (ObjectUtil.isEmpty(id)) {
+			response.setError("id不能为空");
+			return response;
+		}
+		
+		try {
+			cmPaymentService.deleteCmPaymentById(id);
+		} catch (MysqlDBException e) {
+			response.setError("网络连接失败，请检查网络");
+			return response;
+		}
+		response.setResult(Response.RESULT_SUCCESS);
+		return response;
+	}
+	
+	@RequestMapping(value="/findCmPaymentById")
+	@ResponseBody
+	public Response<CmPaymentPO> findCmPaymentById(String id){
+		Response<CmPaymentPO> response =ResponseFactory.getDefaultSuccessResponse();
+		if (ObjectUtil.isEmpty(id)) {
+			response.setError("id不能为空");
+			return response;
+		}
+		try {
+			response.setData(cmPaymentService.getCmPaymentById(id));
+		} catch (MysqlDBException e) {
+			response.setError("网络连接失败，请检查网络");
+			return response;
+		}
+		response.setResult(Response.RESULT_SUCCESS);
+		return response;
+	}
+	
+	@RequestMapping(value="/updateCmPaymentById")
+	@ResponseBody
+	public Response<CmPaymentPO> updateCmPaymentById(HttpServletRequest request) throws ParseException{
+		Response<CmPaymentPO> response =ResponseFactory.getDefaultSuccessResponse();
+		CmPaymentPO condition=new CmPaymentPO();
+        String id = request.getParameter("cmPayment_id");
+		if(!ObjectUtil.isEmpty(id)) condition.setId(Long.valueOf(id));
+        String name = request.getParameter("cmPayment_name");
+		if(!ObjectUtil.isEmpty(name)) condition.setName(String.valueOf(name));
+        String creatTime = request.getParameter("cmPayment_creatTime");
+		if(!ObjectUtil.isEmpty(creatTime)) condition.setCreatTime(dateFormat.parse(creatTime));
+        String departmentId = request.getParameter("cmPayment_departmentId");
+		if(!ObjectUtil.isEmpty(departmentId)) condition.setDepartmentId(String.valueOf(departmentId));
+        String userName = request.getParameter("cmPayment_userName");
+		if(!ObjectUtil.isEmpty(userName)) condition.setUserName(String.valueOf(userName));
+        String state = request.getParameter("cmPayment_state");
+		if(!ObjectUtil.isEmpty(state)) condition.setState(String.valueOf(state));
+		
+		if (ObjectUtil.isEmpty(condition.getId())) {
+			response.setError("id不能为空");
+			return response;
+		}
+
+		try {
+			cmPaymentService.updateCmPayment(condition);
+		} catch (MysqlDBException e) {
+			response.setError("网络连接失败，请检查网络");
+			return response;
+		}
+		response.setResult(Response.RESULT_SUCCESS);
+		return response;
+	}
+	
+	@RequestMapping(value="/addCmPayment")
+	@ResponseBody
+	public Response<CmPaymentPO> addCmPayment(HttpServletRequest request) throws ParseException{
+		Response<CmPaymentPO> response =ResponseFactory.getDefaultSuccessResponse();
+		
+		CmPaymentPO po=new CmPaymentPO();
+        String name = request.getParameter("cmPayment_name");
+		if(!ObjectUtil.isEmpty(name)) po.setName(String.valueOf(name));
+        String creatTime = request.getParameter("cmPayment_creatTime");
+		if(!ObjectUtil.isEmpty(creatTime)) po.setCreatTime(dateFormat.parse(creatTime));
+        String departmentId = request.getParameter("cmPayment_departmentId");
+		if(!ObjectUtil.isEmpty(departmentId)) po.setDepartmentId(String.valueOf(departmentId));
+        String userName = request.getParameter("cmPayment_userName");
+		if(!ObjectUtil.isEmpty(userName)) po.setUserName(String.valueOf(userName));
+        String state = request.getParameter("cmPayment_state");
+		if(!ObjectUtil.isEmpty(state)) po.setState(String.valueOf(state));
+		
+		try {
+			cmPaymentService.addCmPayment(po);
+		} catch (MysqlDBException e) {
+			response.setError("网络连接失败，请检查网络");
+			return response;
+		}
+		response.setResult(Response.RESULT_SUCCESS);
+		return response;
+	}
+}
---
+++ src/main/java/mess/wkb/cm/code/service/CmPaymentService.java
@@ -0,0 +1,183 @@
+package mess.wkb.cm.code.service;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import mess.wkb.cm.tool.bean.Paged;
+import mess.wkb.cm.tool.bean.Query;
+import mess.wkb.cm.tool.web.MysqlDBException;
+import mess.wkb.cm.tool.util.ObjectUtil;
+
+import mess.wkb.cm.code.po.CmPaymentPO;
+
+
+@Service("cmPaymentService")
+public class CmPaymentService extends BaseService<CmPaymentPO>{
+	
+	Logger log = Logger.getLogger(this.getClass());
+	
+	/**
+	 * 新增对象
+	 * @param obj
+	 * @throws MysqlDBException
+	 */
+	public void addCmPayment(CmPaymentPO obj) throws MysqlDBException{
+		if(ObjectUtil.isEmpty(obj)){
+			MysqlDBException e = new MysqlDBException("新增对象为空");
+			log.error("新增对象为空",e);
+			throw e;
+		}
+		this.insert(obj);
+	}
+	
+	/**
+	 * 更新单个对象
+	 * @param obj
+	 * @throws MysqlDBException
+	 */
+	public void updateCmPayment(CmPaymentPO obj) throws MysqlDBException{
+		if(ObjectUtil.isEmpty(obj)){
+			MysqlDBException e = new MysqlDBException("修改对象为空");
+			log.error("修改对象为空",e);
+			throw e;
+		}
+		this.update(obj);
+	}
+	
+	/**
+	 * 通过主键删除对象
+	 * @param id
+	 * @throws MysqlDBException 
+	 */
+	public void deleteCmPaymentById(String id) throws MysqlDBException{
+		if(ObjectUtil.isEmpty(id)){
+			MysqlDBException e = new MysqlDBException("通过主键删除对象，主键 id 不能为空");
+			log.error("通过主键删除对象，主键 id 不能为空",e);
+			throw e;
+		}
+		this.delete(id);
+	}
+	
+	/**
+	 * 通过主键 查询对象
+	 * @param id
+	 * @throws MysqlDBException
+	 */
+	public CmPaymentPO getCmPaymentById(String id) throws MysqlDBException{
+		if(ObjectUtil.isEmpty(id)){
+			MysqlDBException e = new MysqlDBException("通过主键 查询对象，主键 id 不能为空");
+			log.error("通过主键 查询对象，主键 id 不能为空",e);
+			throw e;
+		}
+		return this.get(id);
+	}
+	
+	/**
+	 * 条件查询返回分页列表
+	 * @param pageNo
+	 * @param pageSize
+	 * @param obj
+	 * @return
+	 * @throws MysqlDBException
+	 */
+	@SuppressWarnings("unchecked")
+	public Paged<CmPaymentPO> queryPageCmPayment(Integer pageNo,Integer pageSize,
+			CmPaymentPO obj) throws MysqlDBException{
+		if(ObjectUtil.isEmpty(obj)){
+			MysqlDBException e = new MysqlDBException("查询条件对象为空 - 异常");
+			log.error("查询条件对象为空 - 异常",e);
+			throw e;
+		}
+        Query<CmPaymentPO> q = Query.build(CmPaymentPO.class);
+		q.setPaged(pageNo, pageSize);
+		
+        if(!ObjectUtil.isEmpty(obj.getId()))q.addEq("id", obj.getId()); 
+        if(!ObjectUtil.isEmpty(obj.getName()))q.addEq("name", obj.getName()); 
+        if(!ObjectUtil.isEmpty(obj.getCreatTime()))q.addEq("creatTime", obj.getCreatTime()); 
+        if(!ObjectUtil.isEmpty(obj.getDepartmentId()))q.addEq("departmentId", obj.getDepartmentId()); 
+        if(!ObjectUtil.isEmpty(obj.getUserName()))q.addEq("userName", obj.getUserName()); 
+        if(!ObjectUtil.isEmpty(obj.getState()))q.addEq("state", obj.getState()); 
+        
+		//q.addOrder("createtime", DBOrder.DESC);
+		Paged<CmPaymentPO> page = this.findPagedByQuery(q);
+		return page;
+	}
+	
+	/**
+	 * 条件查询  返回列表
+	 * @param obj
+	 * @return
+	 * @throws MysqlDBException
+	 */
+	@SuppressWarnings("unchecked")
+	public List<CmPaymentPO> queryListCmPaymentByParam(CmPaymentPO obj) throws MysqlDBException{
+		if(ObjectUtil.isEmpty(obj)){
+			MysqlDBException e = new MysqlDBException("查询条件对象为空 - 异常");
+			log.error("查询条件对象为空 - 异常",e);
+			throw e;
+		}
+        Query<CmPaymentPO> q = Query.build(CmPaymentPO.class);
+		
+        if(!ObjectUtil.isEmpty(obj.getId()))q.addEq("id", obj.getId());
+        if(!ObjectUtil.isEmpty(obj.getName()))q.addEq("name", obj.getName());
+        if(!ObjectUtil.isEmpty(obj.getCreatTime()))q.addEq("creatTime", obj.getCreatTime());
+        if(!ObjectUtil.isEmpty(obj.getDepartmentId()))q.addEq("departmentId", obj.getDepartmentId());
+        if(!ObjectUtil.isEmpty(obj.getUserName()))q.addEq("userName", obj.getUserName());
+        if(!ObjectUtil.isEmpty(obj.getState()))q.addEq("state", obj.getState());
+		return this.findByQuery(q);
+	}
+    
+    /**
+	 * 条件查询  返回对象实体
+	 * @param obj
+	 * @return
+	 * @throws MysqlDBException
+	 */
+	@SuppressWarnings("unchecked")
+	public CmPaymentPO getCmPaymentByParam(CmPaymentPO obj) throws MysqlDBException{
+		if(ObjectUtil.isEmpty(obj)){
+			MysqlDBException e = new MysqlDBException("查询条件对象为空 - 异常");
+			log.error("查询条件对象为空 - 异常",e);
+			throw e;
+		}
+        Query<CmPaymentPO> q = Query.build(CmPaymentPO.class);
+		
+        if(!ObjectUtil.isEmpty(obj.getId()))q.addEq("id", obj.getId()); 
+        if(!ObjectUtil.isEmpty(obj.getName()))q.addEq("name", obj.getName()); 
+        if(!ObjectUtil.isEmpty(obj.getCreatTime()))q.addEq("creatTime", obj.getCreatTime()); 
+        if(!ObjectUtil.isEmpty(obj.getDepartmentId()))q.addEq("departmentId", obj.getDepartmentId()); 
+        if(!ObjectUtil.isEmpty(obj.getUserName()))q.addEq("userName", obj.getUserName()); 
+        if(!ObjectUtil.isEmpty(obj.getState()))q.addEq("state", obj.getState()); 
+		return this.get(q);
+	}
+	
+	/**
+	 * 导出excel 数据表
+	 * @param obj
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public List<String[]> queryCmPaymentForExl(CmPaymentPO obj)throws MysqlDBException{
+		if(ObjectUtil.isEmpty(obj)){
+			MysqlDBException e = new MysqlDBException("查询条件对象为空 - 异常");
+			log.error("查询条件对象为空 - 异常",e);
+			throw e;
+		}
+        Query<CmPaymentPO> q = Query.build(CmPaymentPO.class);
+		
+        if(!ObjectUtil.isEmpty(obj.getId()))q.addEq("id", obj.getId()); 
+        if(!ObjectUtil.isEmpty(obj.getName()))q.addEq("name", obj.getName()); 
+        if(!ObjectUtil.isEmpty(obj.getCreatTime()))q.addEq("creatTime", obj.getCreatTime()); 
+        if(!ObjectUtil.isEmpty(obj.getDepartmentId()))q.addEq("departmentId", obj.getDepartmentId()); 
+        if(!ObjectUtil.isEmpty(obj.getUserName()))q.addEq("userName", obj.getUserName()); 
+        if(!ObjectUtil.isEmpty(obj.getState()))q.addEq("state", obj.getState()); 
+		String[] columnProperty = {"id","name","creatTime","departmentId","userName","state"};
+		return this.findExpByQuery(q, columnProperty);
+	}
+	
+	
+	
+}
+
